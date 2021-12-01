12a0c2e07f236bd365a02d5a2daaa5908ef10d3e
wouwou4444 <valery.buz@outlook.com>
2021-12-01 06:52:15 +0100

    Add decode function

--- morse.py
+++ morse.py
@@ -1,5 +1,40 @@
 # morse.py
 # pylint: disable=missing-docstring
 
+
+ALPHABET = {
+    '.-':   'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..':  'D',
+    '.':    'E',
+    '..-.': 'F',
+    '--.':  'G',
+    '....': 'H',
+    '..':   'I',
+    '.---': 'J',
+    '-.-':  'K',
+    '.-..': 'L',
+    '--':   'M',
+    '-.':   'N',
+    '---':  'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.':  'R',
+    '...':  'S',
+    '-':    'T',
+    '..-':  'U',
+    '...-': 'V',
+    '.--':  'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z'
+}
+
 def decode(message):
-    pass # TODO: implement the behavior!
+    if message == "":
+        return ""
+
+    symbols = message.split(" ")
+    letters = [ALPHABET[s] for s in symbols]
+    return ''.join(letters)
