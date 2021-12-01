5017b73b72168bc42cd83cb4f18a37082a8b8bd8
wouwou4444 <valery.buz@outlook.com>
2021-12-01 06:56:12 +0100

    Adding a multi-word test. Red for now

--- tests/test_morse.py
+++ tests/test_morse.py
@@ -88,3 +88,6 @@ class TestMorse(unittest.TestCase):
         self.assertEqual(decode("... --- ..."), "SOS")
 
     # NOTE: we will add a test for *sentences* later
+    def test_whole_sentence(self):
+        message = decode(".- .-.. .-.. / -.-- --- ..- / -. . . -.. / .. ... / -.-. --- -.. .")
+        self.assertEqual(message, "ALL YOU NEED IS CODE")
